027938f7d0887ed774346fb609b4e05f30e5019c
Niemeir <91906070+nniemeir@users.noreply.github.com>
2024-02-14 10:25:51 -0600

    Delete Microsoft directory

--- Microsoft/Development Workstation/Scripts/Enrollment.ps1
+++
@@ -1,4 +0,0 @@
-# Join device to domain
-Write-Host "This system will now be joined to the domain, it will reboot on completion of this task"
-Add-Computer -DomainName universalnoodles.lan -Credential jcarpenter -Restart -Force
-
