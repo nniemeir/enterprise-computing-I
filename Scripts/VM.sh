651764014def1ec27046e9303757d955ad0b5a54
Nat Niemeir <nniemeir@protonmail.com>
2024-05-27 20:07:49 -0500

    Edited variable names for consistency

--- Scripts/VM.sh
+++ Scripts/VM.sh
@@ -1,76 +1,76 @@
 #!/bin/bash
 
-read -p "Enter desired VM parent directory (leave empty for home directory): " VMParentPath
+read -p "Enter desired VM parent directory (leave empty for home directory): " vm_parent_path
 
-if [[ -z "$VMParentPath" ]]; then
-    VMParentPath="$HOME"
+if [[ -z "$vm_parent_path" ]]; then
+    vm_parent_path="$HOME"
 fi
 
 # Remove trailing forward slash if present
-if [ "${VMParentPath: -1}" == "/" ]; then 
-	VMParentPath="${VMParentPath%/*}"
+if [ "${vm_parent_path: -1}" == "/" ]; then 
+	vm_parent_path="${vm_parent_path%/*}"
 fi
 
-VMPath="$VMParentPath/Enterprise Computing I VMs"
+vm_path="$vm_parent_path/Enterprise Computing I VMs"
 
 # Ensure parent directory is writeable before proceeding
-if [ ! -w "$VMParentPath" ]; then 
+if [ ! -w "$vm_parent_path" ]; then 
 	echo "Unable to access the directory. Please ensure that you have write access"
 	exit 1
 fi
 
 # Ensure VMs not deployed already
-if [ -d "$VMPath/RH_pfSense" ]; then
+if [ -d "$vm_path/RH_pfSense" ]; then
 	echo "A deployment already exists in this location"
 	exit 1
 fi
 
-setupVM() {
-local vmName="$1"
-local osType="$2"
+setup_vm() {
+local vm_name="$1"
+local os_type="$2"
 local cpus="$3"
-local memorySize="$4"
-local storageSize="$5"
-local isFirewall="$6"
-local internalNetwork="$7"
-local EFIEnabled="$8"
+local memory_size="$4"
+local storage_size="$5"
+local is_firewall="$6"
+local internal_network="$7"
+local efi_enabled="$8"
 
-VBoxManage createvm --name "$vmName" --ostype "$osType" --register --basefolder "$VMPath" 
-VBoxManage modifyvm "$vmName" --memory "$memorySize" --vram 128
-VBoxManage modifyvm "$vmName" --graphicscontroller vmsvga
-if [ "$isFirewall" = true ] ; then
-VBoxManage modifyvm "$vmName" --nic1 nat 
-VBoxManage modifyvm "$vmName" --nic2 intnet 
-VBoxManage modifyvm "$vmName" --intnet2 $internalNetwork
+VBoxManage createvm --name "$vm_name" --ostype "$os_type" --register --basefolder "$vm_path" 
+VBoxManage modifyvm "$vm_name" --memory "$memory_size" --vram 128
+VBoxManage modifyvm "$vm_name" --graphicscontroller vmsvga
+if [ "$is_firewall" = true ] ; then
+VBoxManage modifyvm "$vm_name" --nic1 nat 
+VBoxManage modifyvm "$vm_name" --nic2 intnet 
+VBoxManage modifyvm "$vm_name" --intnet2 $internal_network
 else 
-VBoxManage modifyvm "$vmName" --nic1 intnet 
-VBoxManage modifyvm "$vmName" --intnet1 "$internalNetwork"
+VBoxManage modifyvm "$vm_name" --nic1 intnet 
+VBoxManage modifyvm "$vm_name" --intnet1 "$internal_network"
 fi
 
-if [ "$EFIEnabled" = true ] ; then
-VBoxManage modifyvm "$vmName" --firmware efi64 
-VBoxManage modifynvram "$vmName" inituefivarstore
-VBoxManage modifynvram "$vmName" enrollmssignatures
-VBoxManage modifynvram "$vmName" enrollorclpk
+if [ "$efi_enabled" = true ] ; then
+VBoxManage modifyvm "$vm_name" --firmware efi64 
+VBoxManage modifynvram "$vm_name" inituefivarstore
+VBoxManage modifynvram "$vm_name" enrollmssignatures
+VBoxManage modifynvram "$vm_name" enrollorclpk
 fi
 
-VBoxManage modifyvm "$vmName" --cpus $cpus
-VBoxManage createhd --filename "$VMPath"/"$vmName"/"$vmName".vdi --size $storageSize --format VDI                     
-VBoxManage storagectl "$vmName" --name "SATA Controller" --add sata --controller IntelAhci       
-VBoxManage storageattach "$vmName" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$VMPath"/"$vmName"/"$vmName".vdi 
+VBoxManage modifyvm "$vm_name" --cpus $cpus
+VBoxManage createhd --filename "$vm_path"/"$vm_name"/"$vm_name".vdi --size $storage_size --format VDI                     
+VBoxManage storagectl "$vm_name" --name "SATA Controller" --add sata --controller IntelAhci       
+VBoxManage storageattach "$vm_name" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$vm_path"/"$vm_name"/"$vm_name".vdi 
 }
 
 # pfSense does not support Secure Boot as of writing
-setupVM "RH_pfSense" "FreeBSD_64" 2 4096 64000 true "linux" false
+setup_vm "RH_pfSense" "FreeBSD_64" 2 4096 64000 true "linux" false
 
-setupVM "RH_freeIPA" "Fedora_64" 2 8192 64000 false "linux" true 
+setup_vm "RH_freeIPA" "Fedora_64" 2 8192 64000 false "linux" true 
 
-setupVM "RH_Ansible" "Fedora_64" 2 2048 64000 false "linux" true
+setup_vm "RH_Ansible" "Fedora_64" 2 2048 64000 false "linux" true
 
-setupVM "RH_DevStation" "Fedora_64" 2 4096 64000 false "linux" true
+setup_vm "RH_DevStation" "Fedora_64" 2 4096 64000 false "linux" true
 
-setupVM "MS_pfSense" "FreeBSD_64" 2 4096 64000 true "windows" false
+setup_vm "MS_pfSense" "FreeBSD_64" 2 4096 64000 true "windows" false
 
-setupVM "MS_AD_Server" "Windows2022_64" 2 8192 100000 true "windows" true
+setup_vm "MS_AD_Server" "Windows2022_64" 2 8192 100000 true "windows" true
 
-setupVM "MS_DevStation" "Windows11_64" 2 8192 80000 true "windows" true
+setup_vm "MS_DevStation" "Windows11_64" 2 8192 80000 true "windows" true
