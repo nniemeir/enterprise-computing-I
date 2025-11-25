1a6c2f54a9020ae761e52b8c66a1b38c8a26ea64
Jacob Niemeir <nniemeir@protonmail.com>
2025-11-25 09:49:57 -0600

    Removed redundancies

--- Scripts/Microsoft/Development Workstation/EnrollAD.ps1
+++
@@ -1,6 +0,0 @@
-. .\..\preferences.ps1
-
-# Join device to domain
-Write-Host "This system will now be joined to the domain, it will reboot on completion of this task"
-Add-Computer -DomainName $Domain -Credential $EnrollUser -Restart -Force
-
